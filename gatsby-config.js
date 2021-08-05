19d932a84c6b3de30f203cc69716c2482ef23e99
Davide Scarioni <47194904+davidescarioni@users.noreply.github.com>
2021-08-05 16:08:03 +0200

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-mdx`,
